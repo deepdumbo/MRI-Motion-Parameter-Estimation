978f4b38eaecafb91811ae5b441acb0d8f44426d
Nalini Singh <nmsingh@mit.edu>
2019-01-17 16:28:39 -0500

    Specify job names and write model summary to text file

--- lib/model.py
+++ lib/model.py
@@ -6,23 +6,39 @@ import data_generator
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse
 import os
+import datetime
 
+parser = argparse.ArgumentParser(description='Train a model to reconstruct images from k-space data.')
+parser.add_argument('--name',help='Name of directories containing checkpoints/tensorboard logs.')
+args = parser.parse_args()
+
+# Set up job name
+job_name = args.name
+if job_name is None:
+    job_name = str(datetime.datetime.now())
+
+# Get current and data directories
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_path = '/data/vision/polina/scratch/nmsingh/imagenet-data'
 
 # Checkpointing
-checkpoint_dir = os.path.join(dir_path,'../training/')
+checkpoint_dir = os.path.join(dir_path,'../training/',job_name)
 checkpoint_name = 'cp-{epoch:04d}.ckpt'
-checkpoint_path = checkpoint_dir+checkpoint_name
-if not os.path.exists(checkpoint_dir):
+checkpoint_path = os.path.join(checkpoint_dir,checkpoint_name)
+if os.path.exists(checkpoint_dir):
+    raise ValueError('Job name has already been used.')
+else:
     os.makedirs(checkpoint_dir)
 cp_callback = keras.callbacks.ModelCheckpoint(
         checkpoint_path, verbose=1, period=5)
 
 # Tensorboard
-tb_dir = os.path.join(dir_path,'../tensorboard/')
-if not os.path.exists(tb_dir):
+tb_dir = os.path.join(dir_path,'../tensorboard/',job_name)
+if os.path.exists(tb_dir):
+    raise ValueError('Job name has already been used.')
+else:
     os.makedirs(tb_dir)
 tb_callback = keras.callbacks.TensorBoard(
         log_dir=tb_dir, histogram_freq=0, write_graph=True, write_images=True)
@@ -43,6 +59,10 @@ model = keras.Sequential([
 model.compile(optimizer=keras.optimizers.RMSprop(lr=0.00002,rho=0.9),
         loss='mean_squared_error',
         metrics=[keras.metrics.mae])
+
+# Write model summary to text file
+with open(os.path.join(checkpoint_dir,'summary.txt'),'w') as fh:
+    model.summary(print_fn=lambda x: fh.write(x+'\n'))
 
 # Load data
 data_path = '/data/vision/polina/scratch/nmsingh/imagenet-data'
