f7eded2a62bc0981a6c9d79a1471cd25a87d801b
Nalini Singh <nmsingh@mit.edu>
2019-01-25 16:30:26 -0500

    Specify network resolution via command line args

--- lib/model.py
+++ lib/model.py
@@ -11,17 +11,19 @@ import os
 import datetime
 
 parser = argparse.ArgumentParser(description='Train a model to reconstruct images from k-space data.')
+parser.add_argument('n',type=int,help ='Dimension, in pixels, to which to crop images.')
 parser.add_argument('--name',help='Name of directories containing checkpoints/tensorboard logs.')
 args = parser.parse_args()
+job_name = args.name
+n = args.n
 
 # Set up job name
-job_name = args.name
 if job_name is None:
     job_name = str(datetime.datetime.now())
 
 # Get current and data directories
 dir_path = os.path.dirname(os.path.realpath(__file__))
-data_path = '/data/vision/polina/scratch/nmsingh/imagenet-data-preprocessed-28'
+data_path = '/data/vision/polina/scratch/nmsingh/imagenet-data-preprocessed-'+str(n)
 
 # Checkpointing
 checkpoint_dir = os.path.join(dir_path,'../training/',job_name)
@@ -44,7 +46,6 @@ tb_callback = keras.callbacks.TensorBoard(
         log_dir=tb_dir, histogram_freq=0, write_graph=True, write_images=True)
 
 # Set up model
-n = 28 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(n,n,2)),
     keras.layers.Dense(2*(n**2), activation=tf.nn.tanh),
