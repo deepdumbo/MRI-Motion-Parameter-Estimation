32f2d5854d0a30303461a338d7cfa512ef55d1e1
Nalini Singh <nmsingh@mit.edu>
2019-02-21 16:40:41 -0500

    Add script to train models from several configs from one command

---
+++ lib/run_experiment.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Run experiment to train several models specified in a list of config files.')
+parser.add_argument('experiment',help = 'Path to .txt experiment file.')
+
+args = parser.parse_args()
+experiment_path = args.experiment
+
+experiment_file = open(experiment_path,'r')
+experiments = experiment_file.read().splitlines()
+
+config_path = '/data/vision/polina/scratch/nmsingh/dev/Motion-Experiments/configs'
+
+for experiment in experiments:
+    this_config = os.path.join(config_path,experiment)
+    subprocess.Popen(['srun -p gpu -t 0 --nodelist=sumac python train.py ' + this_config],shell=True)
