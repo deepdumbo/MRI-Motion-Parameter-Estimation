7213040db55af20648bee93c5484f888fb112499
Nalini Singh <nmsingh@mit.edu>
2019-03-05 20:12:22 -0500

    Rotate, translate, and fill in missing pixels on head and neck images

--- lib/motion.py
+++ lib/motion.py
@@ -6,6 +6,7 @@ import scipy.ndimage as ndimage
 import scipy.signal as signal
 
 import numpy as np
+import cv2
 
 # Induce horizontal translation, within a slice
 def add_horiz_translation(sl,num_pix,k_line,return_k=False):
@@ -47,16 +48,6 @@ def add_rotation(sl,angle,k_line):
     sl_motion = np.fft.ifft2(np.fft.ifftshift(sl_k_combined))
     return sl_rotate, sl_motion
 
-# Induce a rotation and a horizontal translation, within a slice
-def add_rotation_and_translation(sl,angle,num_pix,k_line,return_k=False):
-    sl_rotate = ndimage.rotate(sl, angle, reshape=False)
-    sl_moved = ndimage.interpolation.shift(sl_rotate,[0,num_pix])
-    sl_k = np.fft.fftshift(np.fft.fft2(sl))
-    sl_k_rotate = np.fft.fftshift(np.fft.fft2(sl_moved))
-    sl_k_combined = sl_k
-    sl_k_combined[:,:k_line] = sl_k_rotate[:,:k_line]
-    sl_motion = np.fft.ifft2(np.fft.ifftshift(sl_k_combined))
-    return sl_motion, sl_k_combined
 
 def plot_rotation(img):
     fig,axes = plt.subplots( 4,4, figsize=[12,12] )
@@ -73,6 +64,74 @@ def plot_rotation(img):
             iax.set_ylabel('k-space boundary: '+str(klines[c]))
         if(c==3):
             iax.set_xlabel(str(numdegs[r])+'degree rotation')
+    fig.show()
+
+# Induce a rotation and a horizontal translation, within a slice
+def add_rotation_and_translation(sl,angle,num_pix,k_line,return_k=False):
+    sl_rotate = ndimage.rotate(sl, angle, reshape=False)
+    sl_moved = ndimage.interpolation.shift(sl_rotate,[0,num_pix])
+    sl_k = np.fft.fftshift(np.fft.fft2(sl))
+    sl_k_rotate = np.fft.fftshift(np.fft.fft2(sl_moved))
+    sl_k_combined = sl_k
+    sl_k_combined[:,:k_line] = sl_k_rotate[:,:k_line]
+    sl_motion = np.fft.ifft2(np.fft.ifftshift(sl_k_combined))
+    return sl_motion, sl_k_combined
+
+# Induce a rotation and a horizontal translation, within a slice, and fill it in
+def add_rotation_and_translation(sl,angle,num_pix,k_line,return_k=False):
+    sl_rotate = ndimage.rotate(sl, angle, reshape=False)
+    sl_moved = ndimage.interpolation.shift(sl_rotate,[0,num_pix])
+    sl_k = np.fft.fftshift(np.fft.fft2(sl))
+    sl_k_rotate = np.fft.fftshift(np.fft.fft2(sl_moved))
+    sl_k_combined = sl_k
+    sl_k_combined[:,:k_line] = sl_k_rotate[:,:k_line]
+    sl_motion = np.fft.ifft2(np.fft.ifftshift(sl_k_combined))
+    return sl_motion, sl_k_combined
+
+def get_pixels_to_fill(sl,angle,num_pix):
+    blank = np.zeros(sl.shape)
+    blank_rotate = ndimage.rotate(blank, angle, reshape=False, cval=1)
+    blank_moved = ndimage.interpolation.shift(blank_rotate, [0,-num_pix], cval=1)
+    return blank_moved
+
+# Induce a rotation and a horizontal translation, within a slice, and fill it in
+def add_filled_rotation_and_translation(sl,angle,num_pix,k_line,return_k=False):
+    # Move and segment image
+    sl_rotate = ndimage.rotate(sl, angle, reshape=False)
+    sl_moved = ndimage.interpolation.shift(sl_rotate, [0,-num_pix])
+    seg = get_pixels_to_fill(sl,angle,num_pix)
+    
+    # In-paint slid part of image
+    sl_filled = cv2.inpaint(sl_moved.astype(np.uint16),seg.astype(np.uint8),3,cv2.INPAINT_TELEA)
+    
+    # Combine k-space representations
+    sl_k = np.fft.fftshift(np.fft.fft2(sl))
+    sl_k_filled = np.fft.fftshift(np.fft.fft2(sl_filled))  
+    sl_k_combined = sl_k
+    sl_k_combined[:,:k_line] = sl_k_filled[:,:k_line]
+    sl_motion = np.fft.ifft2(np.fft.ifftshift(sl_k_combined))
+    
+    if(return_k):
+        return sl_filled, sl_motion, sl_k_combined
+    else:
+        return sl_filled, sl_motion
+    
+def plot_filled_motion(img):
+    fig,axes = plt.subplots( 4,4, figsize=[12,12] )
+    numdegs = [0,5,10,20]
+    numpixs = [0,5,10, 15]
+    klines = [90,120,150,180]
+    for i,iax in enumerate( axes.flatten() ):
+        r = i%4
+        c = int((i-r)/4)
+        this_img = add_filled_rotation_and_translation(img,numdegs[r],numpixs[r],klines[c])[1]
+        iax.imshow(np.real(this_img),cmap='gray', interpolation='nearest')
+        iax.set_yticks([])
+        iax.set_xticks([])
+        if(r==0):
+            iax.set_ylabel('k-space boundary: '+str(klines[c]))
+        if(c==3):
+            iax.set_xlabel(str(numpixs[r])+'-px, '+str(numdegs[r])+'-degree')
     fig.show()
 
 # Induce translation, through multiple slices
